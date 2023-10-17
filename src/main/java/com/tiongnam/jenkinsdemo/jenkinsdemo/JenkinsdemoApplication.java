e2eb5344e56bea3e8db72cce6b01d807fa99f893
TIONGNAM\faizal.azlee <faizal.azlee@tiongnam.com.my>
2023-10-17 11:13:51 +0800

    initial commit

---
+++ src/main/java/com/tiongnam/jenkinsdemo/jenkinsdemo/JenkinsdemoApplication.java
@@ -0,0 +1,21 @@
+package com.tiongnam.jenkinsdemo.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@SpringBootApplication
+public class JenkinsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsdemoApplication.class, args);
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println("welcome to jenkin demo. the timeno is --> "+dtf.format(now));
+	}
+
+}
---
+++ src/main/java/com/tiongnam/jenkinsdemo/jenkinsdemo/controller/demoController.java
@@ -0,0 +1,14 @@
+package com.tiongnam.jenkinsdemo.jenkinsdemo.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value="/demo")
+public class demoController {
+    @GetMapping("/")
+    public @ResponseBody String getBook() {
+        return "Welcome Demo";
+    }
+
+}
