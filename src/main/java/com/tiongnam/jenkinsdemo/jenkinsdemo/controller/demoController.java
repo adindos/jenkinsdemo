62263ee35c588eec68027fcda0826fbbcd349ef1
TIONGNAM\faizal.azlee <faizal.azlee@tiongnam.com.my>
2023-10-19 22:42:32 +0800

    demo new

--- src/main/java/com/tiongnam/jenkinsdemo/jenkinsdemo/controller/demoController.java
+++ src/main/java/com/tiongnam/jenkinsdemo/jenkinsdemo/controller/demoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 public class demoController {
     @GetMapping("/")
     public @ResponseBody String getBook() {
-        return "Welcome Demo Edit";
+        return "Welcome Demo New";
     }
 
 }
